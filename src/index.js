e86a92f3b016b18abc512bc83d20357b1aa87c52
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-11-02 21:48:30 -0500

    g

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
