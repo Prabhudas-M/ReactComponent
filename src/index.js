4891eee656cd5222cc2bdd66b692e347e37f1c54
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-11-02 21:31:56 -0500

    q

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Heading from "./Heading";
-
+import List from "./List";
 ReactDOM.render(
   <div>
     <Heading />
+    <List />
+<List />
   </div>,
   document.getElementById("root")
 );
